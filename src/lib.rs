3e137bafd0c0ebe6ac38dd24f0eb22b111261fe6
Kyle Smith <kyleafmine@gmail.com>
2024-06-24 15:46:41 -0300

    Re-export some constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![feature(generic_const_exprs)]
 pub mod device;
 pub mod graph;
+pub mod signal;
+
+pub mod constants {
+    pub use crate::bindings::mpr_dir;
+    pub use crate::bindings::mpr_type;
+}
 mod bindings;

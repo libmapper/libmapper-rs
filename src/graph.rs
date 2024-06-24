841a80a9bc6286d58c7c0f880b7db1d4594b5489
Kyle Smith <kyleafmine@gmail.com>
2024-06-24 14:12:42 -0300

    Add Graph struct

---
+++ src/graph.rs
@@ -0,0 +1,44 @@
+use crate::{bindings::*, device::Device};
+
+/// A graph is a lightweight connection to libmapper's distributed graph.
+/// You can use a graph to create maps and query the state of the graph.
+pub struct Graph {
+    pub(crate) handle: mpr_graph,
+    pub(crate) devices: Vec<Device>,
+    owned: bool
+}
+
+impl Graph {
+  pub fn create() -> Graph {
+    Graph {
+      owned: true,
+      devices: Vec::new(),
+      handle: unsafe { mpr_graph_new(0) }
+    }
+  }
+}
+
+impl Drop for Graph {
+    fn drop(&mut self) {
+        if self.owned {
+            unsafe {
+                mpr_graph_free(self.handle);
+            }
+        }
+    }
+}
+
+impl Graph {
+  /// Poll the graph without blocking
+  pub fn poll(&self) {
+    unsafe {
+      mpr_graph_poll(self.handle, 0);
+    }
+  }
+
+  pub fn poll_and_block(&self, time: i32) {
+    unsafe {
+      mpr_graph_poll(self.handle, time);
+    }
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod device;
+pub mod graph;
 mod bindings;

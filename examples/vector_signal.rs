2052c44d392d40026e5709baae9280e883225fb0
Kyle Smith <kyleafmine@gmail.com>
2024-06-25 13:52:26 -0300

    Add vector signal example

---
+++ examples/vector_signal.rs
@@ -0,0 +1,34 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use libmapper_rs::{constants::mpr_dir, device::Device};
+
+fn main() {
+    let dev = Device::create("rustmapper");
+    loop {
+        dev.poll_and_block(10);
+        if dev.is_ready() {
+            break;
+        }
+    }
+
+    println!("Device became ready!");
+    let mut sig = dev.create_vector_signal::<f64>("test_sin", mpr_dir::MPR_DIR_OUT, 2);
+    let debug_sig = dev.create_vector_signal::<f64>("debug_msg", mpr_dir::MPR_DIR_IN, 2);
+    loop {
+        dev.poll_and_block(100);
+        let time = (SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64) as f64
+            / 1000f64;
+
+        let values = [time.sin(), time.cos()];
+        sig.set_value(&values);
+        if debug_sig.get_status().was_set_remote() {
+            println!(
+                "Received debug message: {:?}",
+                debug_sig.get_value::<f64>().unwrap().0
+            );
+        }
+    }
+}

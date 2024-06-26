64fdb7d4d9199fd6a1aa2fe35ef2a0d011e8561f
Kyle Smith <kyleafmine@gmail.com>
2024-06-26 11:47:04 -0300

    chore: Format examples

--- examples/maps.rs
+++ examples/maps.rs
@@ -1,7 +1,5 @@
 use libmapper_rs::{device::Device, graph::Map};
 
-
-
 pub fn main() {
     let dev = Device::create("rustmapper");
     loop {
@@ -12,22 +10,25 @@ pub fn main() {
     }
 
     println!("Device became ready!");
-    let mut sig_a = dev.create_signal::<i32>("output", libmapper_rs::constants::mpr_dir::MPR_DIR_OUT);
+    let mut sig_a =
+        dev.create_signal::<i32>("output", libmapper_rs::constants::mpr_dir::MPR_DIR_OUT);
     let sig_b = dev.create_signal::<i32>("input", libmapper_rs::constants::mpr_dir::MPR_DIR_IN);
     let map = Map::create(&sig_a, &sig_b);
     map.push();
     loop {
         dev.poll_and_block(100);
         if map.is_ready() {
-          break;
+            break;
         }
     }
     println!("Map created!");
     for i in 0..100 {
-      sig_a.set_value_single(&i);
-      dev.poll_and_block(10);
-      let val = sig_b.get_value_single::<i32>().expect("Signal didn't send!");
-      println!("Sent: {}, Received: {}", i, val.0);
-      assert_eq!(i, val.0)
+        sig_a.set_value_single(&i);
+        dev.poll_and_block(10);
+        let val = sig_b
+            .get_value_single::<i32>()
+            .expect("Signal didn't send!");
+        println!("Sent: {}, Received: {}", i, val.0);
+        assert_eq!(i, val.0)
     }
 }
--- examples/signal.rs
+++ examples/signal.rs
@@ -3,23 +3,31 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use libmapper_rs::{constants::mpr_dir, device::Device};
 
 fn main() {
-  let dev = Device::create("rustmapper");
-  loop {
-      dev.poll_and_block(10);
-      if dev.is_ready() {
-          break;
-      }
-  }
+    let dev = Device::create("rustmapper");
+    loop {
+        dev.poll_and_block(10);
+        if dev.is_ready() {
+            break;
+        }
+    }
 
-  println!("Device became ready!");
-  let mut sig = dev.create_signal::<f64>("test_sin", mpr_dir::MPR_DIR_OUT);
-  let debug_sig = dev.create_signal::<f64>("debug_msg", mpr_dir::MPR_DIR_IN);
-  loop {
-      dev.poll_and_block(100);
-      let time = ((SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as u64) as f64 / 1000f64).sin();
-      sig.set_value_single(&time);
-      if debug_sig.get_status().was_set_remote() {
-          println!("Received debug message: {:?}", debug_sig.get_value_single::<f64>().unwrap());
-      }
-  }
+    println!("Device became ready!");
+    let mut sig = dev.create_signal::<f64>("test_sin", mpr_dir::MPR_DIR_OUT);
+    let debug_sig = dev.create_signal::<f64>("debug_msg", mpr_dir::MPR_DIR_IN);
+    loop {
+        dev.poll_and_block(100);
+        let time = ((SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64) as f64
+            / 1000f64)
+            .sin();
+        sig.set_value_single(&time);
+        if debug_sig.get_status().was_set_remote() {
+            println!(
+                "Received debug message: {:?}",
+                debug_sig.get_value_single::<f64>().unwrap()
+            );
+        }
+    }
 }

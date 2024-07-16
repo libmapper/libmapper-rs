3c37c49ae0bf2003a7e49320693d0a416f4f67eb
Kyle Smith <kyleafmine@gmail.com>
2024-07-16 11:09:03 -0300

    Cleanup imports

--- src/object.rs
+++ src/object.rs
@@ -1,6 +1,6 @@
 use std::{ffi::c_void, ptr};
 
-use crate::{bindings::{mpr_obj, mpr_obj_get_type, mpr_obj_set_prop, mpr_prop, mpr_type}, device::MappableType, signal::Signal};
+use crate::{bindings::{mpr_obj_get_type, mpr_obj_set_prop, mpr_prop, mpr_type}, device::MappableType, signal::Signal};
 
 pub trait AsMprObject {
   fn as_mpr_object(&self) -> *mut c_void;

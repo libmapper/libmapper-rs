acb1c372ab4e3f0eb774619ad00cf33df5fc915d
Kyle Smith <kyleafmine@gmail.com>
2024-07-25 11:42:36 -0300

    Don't run build script on docs.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() {
+    if std::env::var("DOCS_RS").is_ok() {
+        return; // don't try to find libmapper on docs.rs
+    }
+
     pkg_config::Config::new()
         .atleast_version("2.4.6")
         .probe("libmapper")

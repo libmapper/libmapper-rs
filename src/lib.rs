877cc106be96a008b929e1583ce409912afe443d
Kyle Smith <kyleafmine@gmail.com>
2024-07-31 13:55:43 -0300

    Add crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! # libmapper-rs
+//! 
+//! libmapper is a cross-platform library for connecting data sources to synthesizers, DAWs, and other hardware or software devices.
+//! It provides a simple API for creating and managing signals, devices, and mappings between them.
+//! 
+//! This project contains safe, idiomatic rust bindings to the libmapper C api.
+//! 
+//! ## Concepts
+//! ### [Devices](device)
+//! Libmapper operates on a shared peer-to-peer graph. A [Device](device::Device) represents a connection to this graph and is a container for signals.
+//! 
+//! Most libmapper code will start by creating a device and polling until it becomes ready, like so:
+//! ```
+//! use libmapper_rs::device::Device;
+//! fn main() {
+//!     let mut device = Device::new("CoolDevice").unwrap();
+//!     loop {
+//!       device.poll_and_block(10);
+//!       if device.is_ready() {
+//!          break;
+//!         }
+//!     }
+//!     println!("Device is ready!");
+//!     // create signals, maps, etc.
+//! }
+//! ```
+
 use bindings::mpr_get_version;
 
 pub mod device;

8d46be2cdd5399619f4342ce93aa7a4ef06211be
Kyle Smith <kyleafmine@gmail.com>
2024-08-21 12:32:49 -0300

    Update build script to not use pkg-config on windows

--- build.rs
+++ build.rs
@@ -3,8 +3,13 @@ fn main() {
         return; // don't try to find libmapper on docs.rs
     }
 
-    pkg_config::Config::new()
-        .atleast_version("2.4.9")
-        .probe("libmapper")
-        .unwrap();
+    if cfg!(windows) {
+        println!("cargo:rustc-link-lib=libmapper");
+        println!("cargo:rustc-link-lin=liblo");
+    } else {
+        pkg_config::Config::new()
+            .atleast_version("2.4.9")
+            .probe("libmapper")
+            .unwrap();
+    }
 }

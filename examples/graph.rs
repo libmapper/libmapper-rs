8eb548ae474e851c149fbb22f00517c01b95e540
Kyle Smith <kyleafmine@gmail.com>
2024-08-26 12:58:08 -0300

    Cleanup graph.rs example

--- examples/graph.rs
+++ examples/graph.rs
@@ -7,17 +7,22 @@ use libmapper_rs::{constants::{mpr_prop, mpr_type}, graph::Graph, object::Mapper
 fn main() {
   let graph = Graph::create();
   let mut count = 0;
+
+  // Have to subscribe to all devices first in order to discover devices we don't own!
   graph.subscribe(None, &[mpr_type::MPR_DEV]);
 
   loop {
+
     graph.poll_and_block(Duration::from_millis(10));
     let list = graph.get_devices();
+
     if list.len() != 0 {
       for dev in list {
         println!("Found device: {:?}", dev.get_property_str(mpr_prop::MPR_PROP_NAME).unwrap());
       }
       break;
     }
+    
     println!("Loop {}", count);
     count += 1;
   }

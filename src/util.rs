f20dfa8153884495a5528a56e3d6a761d1712627
Kyle Smith <kyleafmine@gmail.com>
2024-08-28 12:27:41 -0300

    Optimize `read_list` utility function

--- src/util.rs
+++ src/util.rs
@@ -7,15 +7,10 @@ use crate::bindings::{mpr_list_get_next, mpr_list_get_size};
 /// `constructor` is passed a pointer to the backing libmapper object and should return a new wrapped instance of the item.
 pub fn read_list<T, J: Fn(*mut c_void) -> T>(list: *mut *mut c_void, constructor: J) -> Vec<T> {
   let mut ptr = list;
-  let len = unsafe { mpr_list_get_size(ptr) };
 
-  if len == 0 {
-    return Vec::new();
-  }
+  let mut values = Vec::new();
 
-  let mut values = Vec::with_capacity(len as usize);
-
-  for _ in 0..len {
+  while !ptr.is_null() {
     let val = constructor(unsafe {*ptr});
     values.push(val);
 

cbad9da116e0b01e6ed48d8de3545aaa8c2a5a19
Kyle Smith <kyle@cabotmc.dev>
2026-06-23 12:04:32 -0300

    update constants for 2.6

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-use std::{env, path::PathBuf};
-
 fn main() {
     if std::env::var("DOCS_RS").is_ok() {
         return; // don't try to find libmapper on docs.rs
@@ -9,23 +7,9 @@ fn main() {
         println!("cargo:rustc-link-lib=libmapper");
         println!("cargo:rustc-link-lib=liblo");
     } else {
-        let library = pkg_config::Config::new()
-            .atleast_version("2.4.9")
+        pkg_config::Config::new()
+            .atleast_version("2.6")
             .probe("libmapper")
             .unwrap();
-
-        let bindings = bindgen::Builder::default()
-            .header("wrapper.h")
-            .clang_args(library.include_paths.iter().map(|x| format!("-I{}", x.to_str().unwrap())))
-            .rustified_enum("mpr.*")
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-            .generate()
-            .expect("Unable to generate bindings");
-
-            // Write the bindings to the $OUT_DIR/bindings.rs file.
-            let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-            bindings
-                .write_to_file(out_path.join("bindings.rs"))
-                .expect("Couldn't write bindings!");
     }
 }
